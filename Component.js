56ffc50f49a7aa983a8c22421a9b002a71ad0f85
PunitDh <cas012115@coderacademy.edu.au>
2021-08-17 16:23:07 +1000

    Add styling to component

--- Component.js
+++ Component.js
@@ -14,9 +14,11 @@ export default function Component({ propCount, buttonElem }) {
 	}, [buttonElem])
 	
 	return `
-		This page looks very basic as it is simply a button that increments a counter. However, underneath the hood, it uses a custom React-like clone to manage state and hooks. You can find the source code <a href="https://github.com/PunitDh/Custom-React">here.</a>
-		State: ${count} This increments the state using useState <br />
-		Prop: ${propCount} This changes the prop using useEffect <br />
-		Prop Doubled:  ${propCountDoubled} This keeps track of the prop value and doubles it.
+		<h3>This page looks very basic as it is simply a button that increments a counter.</h3>
+		<h3>However, underneath the hood, it uses a custom React-like clone to manage state and hooks.<h3>
+		<h4>You can find the source code <a href="https://github.com/PunitDh/Custom-React">here.</a></h4>
+		<p><b>State: ${count}</b> <small>This increments the state using useState </small></p>
+		<p><b>Prop: ${propCount}</b> <small>This changes the prop using useEffect </small></p>
+		<p><b>Prop Doubled:  ${propCountDoubled}</b> <small>This keeps track of the prop value and doubles it.</small></p>
 	`
 }
