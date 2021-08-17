4ecbbc2b2399b96b1aa59270e640bf5a8ff68455
PunitDh <cas012115@coderacademy.edu.au>
2021-08-17 15:54:58 +1000

    Create custom React project

---
+++ Component.js
@@ -0,0 +1,21 @@
+import { useState, useEffect, useMemo } from "./MyReact.js";
+
+export default function Component({ propCount, buttonElem }) {
+
+	const [count, setCount] = useState(0)
+	const propCountDoubled = useMemo(() => {
+		return propCount * 2
+	}, [propCount])
+
+	useEffect(() => {
+		const handler = () => setCount(currentCount => currentCount + 1)
+		buttonElem.addEventListener("click", handler)
+		return () => buttonElem.removeEventListender("click", handler)
+	}, [buttonElem])
+	
+	return `
+		State: ${count}
+		Prop: ${propCount}
+		Prop Doubled: ${propCountDoubled}
+	`
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+import { render } from "./MyReact.js"
+import Component from "./Component.js"
+
+let propCount = 0
+document.getElementById('btn-prop').addEventListener("click", () => {
+	propCount++
+	renderComponent()
+})
+
+function renderComponent() {
+	render(Component, { propCount, buttonElem: document.getElementById("btn-count-2") }, document.getElementById("root"))
+}
+
+renderComponent()
